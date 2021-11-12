caed64ed26907687f2afaaabd176cf198d3022f2
chavda-bhavik <bhavik.chvda@gmail.com>
2021-11-12 16:23:43 +0530

    added tailwindcss, daisyui and prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <main>
+            <h1>
+                Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+            <button className="btn btn-primary btn-sm">Daisyz Ui Button</button>
+        </main>
+    );
+};
+
+export default Home;
